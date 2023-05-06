7957d9037086585d4f1cbb8cfbcb8cda725232ed
Prajeeth <bprajeeth285@gmail.com>
2023-05-06 18:42:07 +0530

    new commit

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
     //ev.target.style.backgroundImage=document.getElementById(data);
 }
+
+
+function faces(){
+    var x=``;
+    var faceloc=document.getElementById("imagestorage");
+    for(let i=1; i<9;i++)
+    {
+        x=x+`<img class="headimg" id="drag${i}" src="\\assets\\faces\\head${i}.png"
+        draggable="true" ondragstart="drag(event)">`;
+    }
+    faceloc.innerHTML=x;
+}
+
+faces();
